aefc984e4ae7d8b80eb258589f849c698f311896
AelinaXY <Lauren@2infinity.net>
2023-04-24 15:37:17 +0100

    Change pokemon

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const leftPad = require("left-pad");
+const axios = require("axios");
 const calc = require("./calc");
 
-console.log(leftPad("Hello World", 60));
+// console.log(leftPad("Hello World", 60));
 
-console.log(`${calc.add(5,5)} | ${calc.sub(5,5)} | ${calc.mult(5,5)} | ${calc.div(5,5)}`);
+// console.log(`${calc.add(5,5)} | ${calc.sub(5,5)} | ${calc.mult(5,5)} | ${calc.div(5,5)}`);
+
+axios.get('https://pokeapi.co/api/v2/pokemon/meowscarada')
+.then(function (response) { console.log(response.data.name +'\n' +
+response.data.stats.forEach((m) => (console.log(`STAT: ${m.stat.name} | BASE: ${m.base_stat}`))));});
