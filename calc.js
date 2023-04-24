08eaa1e267bb492053500bc6eaf5904d9a44098b
AelinaXY <Lauren@2infinity.net>
2023-04-24 14:55:37 +0100

    Add calc

---
+++ calc.js
@@ -0,0 +1,12 @@
+let add = (a,b) => (a+b);
+let sub = (a,b) => (a-b);
+let mult = (a,b) => (a*b);
+let div = (a,b) => (a/b);
+
+function add2(a,b)
+{
+    return a+b;
+}
+module.exports = {
+    add,sub,mult,div,add2
+}
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+const leftPad = require("left-pad");
+const calc = require("./calc");
+
+console.log(leftPad("Hello World", 60));
+
+console.log(`${calc.add(5,5)} | ${calc.sub(5,5)} | ${calc.mult(5,5)} | ${calc.div(5,5)}`);
